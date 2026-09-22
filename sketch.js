ce772debbc532bf8b1ad9c2a78722e9e4eed372c
kersonik <sumsalpepik@gmail.com>
2026-09-22 10:20:30 +0200

    Update linky function and add caryMary function
    Modified the linky function to change line positions and added caryMary function for random colored lines.

--- sketch.js
+++ sketch.js
@@ -3,13 +3,29 @@ const menuItems = ["Linky", "Čáry máry", "Šachovnice"];
  function linky() {
   stroke(1);
   strokeWeight(5);
-  let x = 40;
-  let x2 = 750;
-  let y = 40;
-  for (let i = 40; i < height; i += 40) {
+  let x = 0;
+  let x2 = 800;
+  for (let i = 10; i < height; i += 20) {
   line(x, i, x2, i);
   }
 } 
+
+function caryMary() {
+  stroke(random(255), random(255), random(255));
+  strokeWeight(random(5, 25));
+
+  for (let i = 0; i < 10; i++) {
+    let x1 = random(width);
+    let y1 = random(height);
+    let x2 = random(width);
+    let y2 = random(height);
+    line(x1, y1, x2, y2);
+  }
+}
+
+function sachovnice() {
+  
+}
 
 function setup() {
   createCanvas(800, 800);
